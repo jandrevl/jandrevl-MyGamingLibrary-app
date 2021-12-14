3e469a9dd479e1fe80b48bad332fd493c432543f
Joao Lopes <jandrevl@gmail.com>
2021-12-14 12:02:01 +0000

    continuing work on game details component. comments are working.

--- frontend/MyGamingLibrary-App/src/app/components/game-details/game-details.component.ts
+++ frontend/MyGamingLibrary-App/src/app/components/game-details/game-details.component.ts
@@ -2,13 +2,13 @@ import { CommentService } from './../../services/comment.service';
 import { AuthenticationService } from './../../services/authentication.service';
 import { GameDetailsAuxService } from './../../services/game-details-aux.service';
 import { GamesService } from './../../services/games.service';
-import { Component, OnInit, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute, Navigation, Router } from '@angular/router';
 import { Game } from 'src/app/models/game.model';
 import { MatCarousel, MatCarouselComponent } from '@ngmodule/material-carousel';
 import { Observable } from 'rxjs';
 import { NgForm } from '@angular/forms';
-import { GameComment } from 'src/app/models/game-comment.model';
+import { GameComment, GameCommentDTO } from 'src/app/models/game-comment.model';
 
 @Component({
   selector: 'app-game-details',
@@ -23,21 +23,22 @@ export class GameDetailsComponent implements OnInit {
   id: number = 0;
   gameString: string = '';
   screenshots: string[];
-  comments: GameComment[] = []  
+  comments!: Observable<GameComment[]>
   
   
 
   constructor(
     private router: Router,
     private gameDetailsAuxService: GameDetailsAuxService,
-    private commentService: CommentService
+    private commentService: CommentService,
+    // private changeDetection: ChangeDetectorRef
     
   ) {
     this.game = this.gameDetailsAuxService.game;
     this.screenshots = this.game.screenshots;
-    this.commentService.getCommentsByGameId(this.game.id).subscribe(
-      result => {this.comments = result}
-    )
+    // this.commentService.getCommentsByGameId(this.game.id).subscribe(
+    //   result => {this.comments = result.slice().reverse()}
+    // )
    }
   
 
@@ -46,15 +47,42 @@ export class GameDetailsComponent implements OnInit {
     console.log(this.game.name);
     console.log(this.game.detail);
     // this.isLoggedIn$ = this.authenticationService.isLoggedIn;
+    this.reloadCommentsList();
   }
 
   submitComment() {
+    let currentUser = JSON.parse(sessionStorage.getItem('currentUser')!);
+    
+    let gameCommentDTO: GameCommentDTO = {
+      gameId: this.game.id,
+      username: currentUser.username,
+      date: new Date(),
+      comment: this.commentForm.value.commentText
+    }
+    this.commentService.postComment(gameCommentDTO).subscribe(result => {
+      console.log(result)
+      this.reloadCommentsList()
+    })
+    this.commentForm.reset();
+    // this.reloadCommentsList();
+    // this.changeDetection.detectChanges();
 
+  }
+
+  // updateCommentsList(): void {
+  //   this.commentService.getCommentsByGameId(this.game.id).subscribe(
+  //     result => {this.comments = result.slice().reverse()})
+  // }
+
+  reloadCommentsList() {
+    this.comments = this.commentService.getCommentsByGameId(this.game.id);
   }
 
   isLoggedIn(): boolean {
     if(sessionStorage.getItem('currentUser')) { return true }
      return false;
   }
+
+  
 
 }
